b1091dfacb964de595a283b24fca33d20c3f6b02
Christian <misterchristian4@gmail.com>
2026-05-20 22:23:42 +0000

    add neon db

--- app.py
+++ app.py
@@ -19,8 +19,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
+database_url = os.environ.get('DATABASE_URL')
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
 # Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///database.db'
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-key-only')
 
